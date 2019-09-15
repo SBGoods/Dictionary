b7603c9ed0b91a292702080e23d36f0c6f40af82
SBGoods <selena_goods@yahoo.com>
2019-09-15 18:18:01 -0400

    First commit - created SearchFragment, MainViewModel, and MainActivity

---
+++ app/src/main/java/com/goods/android/dictionary/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.goods.android.dictionary.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/goods/android/dictionary/ui/main/SearchFragment.kt
@@ -0,0 +1,37 @@
+package com.goods.android.dictionary.ui.main
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.goods.android.dictionary.R
+import com.goods.android.dictionary.databinding.SearchFragmentBinding
+
+class SearchFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SearchFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: SearchFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val binding = DataBindingUtil.inflate<SearchFragmentBinding>(inflater, R.layout.search_fragment, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
